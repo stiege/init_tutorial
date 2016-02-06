45b4f2a01d3fc098a34c7d7edd00879f4e9f3f1e
Alex Hodge <alex.hodge@gmail.com>
2016-02-06 17:19:53 +1300

    Remove dependency from main on init functions
    * __init_start and __init_end moved to inside linker
      scripts.

--- src/main.c
+++ src/main.c
@@ -1,20 +1,20 @@
 #include "stdio.h"
 #include "init_defs.h"
 
-extern init_func ** __init_start;
-extern init_func ** __init_end;
+extern init_func * __init_start;
+extern init_func * __init_end;
 
 void main(void)
 {
-    init_func ** inits = __init_start;
+    init_func ** inits = &__init_start;
 
-    if (NULL != __init_start)
+    if (__init_start != __init_end)
     {
         printf("%s\n", "Beginning Init");
         do
         {
-            (*inits++)();
-        }while (inits <= __init_end);
+            (**inits++)();
+        } while (inits < &__init_end);
         printf("%s\n", "End of init");
     }
     else
--- src/requires_init.c
+++ src/requires_init.c
@@ -6,15 +6,13 @@ void init1(void)
     printf("%s\n", "init1 called");
 }
 
-void init2(void)
+void init2 (void)
 {
     printf("%s\n", "init2 called");
 }
 
+__attribute__((section(".init_functions")))
 init_func * init_array[] = {
     &init1,
     &init2
 };
-
-init_func ** __init_start = &init_array[0];
-init_func ** __init_end = &init_array[1];
